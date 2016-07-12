16406ac25fafa6b573b5f50a3b0492e0f7a29f51
whlsxl <whlsxl@users.noreply.github.com>
2016-07-13 03:55:32 +0800

    add adjust cmts function

--- jd_auto.js
+++ jd_auto.js
@@ -123,6 +123,31 @@ JDA.setTagsCount = function(count) {
   return JDA.setConfig('JDA_tags_count', count);
 }
 
+JDA.getCmtsType = function() {
+  var type = JDA.getConfig("JDA_cmts_type");
+  if (type == "") {
+    return 'random';
+  }
+  return type
+}
+
+JDA.setCmtsType = function(type) {
+  JDA.setConfig("JDA_cmts_type", type);
+}
+
+JDA.getOrderCmt = function() {
+  var defaultOrder = JDA.getConfig('JDA_order_cmt');
+  var defaultOrderInt = parseInt(defaultOrder);
+  if (defaultOrder == '' || JDA.getCmts()[defaultOrderInt] == null) {
+    return 0
+  }
+  return defaultOrderInt;
+}
+
+JDA.setOrderCmt = function(order) {
+  return JDA.setConfig('JDA_order_cmt', order);
+}
+
 JDA.setCmtTextarea = function(textarea, text = null) {
   if (text == null) {
     var ns = JDA.getCmts()
@@ -131,18 +156,32 @@ JDA.setCmtTextarea = function(textarea, text = null) {
   $(textarea).text(text)
 };
 
-JDA.setAllCmtTextarea = function(text = null) {
+JDA.setAllCmtTextarea = function() {
+  var type = JDA.getCmtsType();
+  var order = JDA.getOrderCmt();
+  var cmts = JDA.getCmts();
   $('.f-textarea').find('textarea').each(function(index, el) {
-    JDA.setCmtTextarea(this);
+    if (type == 'random') {
+      JDA.setCmtTextarea(this);
+    } else {
+      JDA.setCmtTextarea(this, cmts[order]);
+    }
   });
 }
 
-JDA.cmtsSelectNodeString = function() {
-  var nodeString = '<div class="jda_auto_cmt_div" style="padding-left: 30px; padding-top: 18px; float: left; "><select class="jda_auto_cmt">';
+JDA.cmtsSelectNodeString = function(wrapper = true) {
+  var nodeString = ''
+  if (wrapper) {
+    nodeString = '<div class="jda_auto_cmt_div" style="padding-left: 30px; padding-top: 18px; float: left; ">';
+  }
+  nodeString += '<select class="jda_auto_cmt">';
   $.each(JDA.getCmts(), function(index, value) {
-    nodeString += '<option>' + value + '</option>';
+    nodeString += '<option data-order="' + index + '">' + value + '</option>';
   });
-  nodeString += '</select></div>';
+  nodeString += '</select>';
+  if (wrapper) {
+    nodeString += '</div>';
+  }
   return nodeString;
 };
 
@@ -186,39 +225,57 @@ JDA.setTags = function(from = "body", type = null, count = -1) {
 
 JDA.addMenuBar = function() {
   // stars
-  $('.f-btnbox').append('<div class="jda-menu-bar-star" style="float:right; margin-top: 4px; padding-left: 30px; "><div style="float:left; font-size:15px">全部评分：</div><br><span class="commstar z-star-checked"><span class="star star1"><i class="face"></i></span><span class="star star2"><i class="face"></i></span><span class="star star3"><i class="face"></i></span><span class="star star4"><i class="face"></i></span><span class="star star5"><i class="face"></i></span><span class="star-info">0分</span></span></div>');
-  JDA.startAll($('.f-btnbox'), JDA.getDefaultStar());
-  $('.f-btnbox').find('.star').click(function(event) {
+  $('.f-btnbox').append('<div class="jda_options_wrapper" style="float:right; margin-top: 4px;"></div>')
+  var appendNode = '.jda_options_wrapper';
+  $(appendNode).append('<div class="jda-menu-bar-star" style="float:right; margin-top: 4px; padding-left: 30px; "><div style="float:left; font-size:15px">全部评分：</div><br><span class="commstar z-star-checked"><span class="star star1"><i class="face"></i></span><span class="star star2"><i class="face"></i></span><span class="star star3"><i class="face"></i></span><span class="star star4"><i class="face"></i></span><span class="star star5"><i class="face"></i></span><span class="star-info">0分</span></span></div>');
+  JDA.startAll($(appendNode), JDA.getDefaultStar());
+  $('.jda-menu-bar-star').find('.star').click(function(event) {
     var star = parseInt($(this).siblings('.star-info').text());
     JDA.startAll('body', star, true);
     JDA.setDefaultStar(star);
   });
   // tags type
-  $('.f-btnbox').append('<div class="dd jda-tag-options" style="float:right; margin-top: 9px; "><span style="float:left; padding-right:10px; font-size:15px; padding-top:2px;">标签选项:</span><div class="item" data-type="random"><a href="#none">随机标签</a></div> <div class="item" data-type="ordered"><a href="#none">顺序标签</a></div></div>');
-  $('.f-btnbox').append('<style type="text/css">' +
-    '.jda-tag-options .item {margin: 2px 8px 2px 0; float: left;}' +
-    '.jda-tag-options .item a {padding:6px 8px; border:1px solid #ccc;  background: #fff; font-size: 13px;}' +
-    '.jda-tag-options .item.selected a {border: 2px solid #e4393c; padding: 4px 6px;}' +
-    '</style>');
-  $('.f-btnbox a').click(function(e) {
+  $(appendNode).append('<div class="dd jda-tag-options jda-options" style="float:right; margin-top: 9px; "><span style="float:left; padding-right:10px; font-size:15px; padding-top:2px;">标签选项:</span><div class="item" data-type="random"><a href="#none">随机标签</a></div> <div class="item" data-type="ordered"><a href="#none">顺序标签</a></div></div>');
+  $('.jda-tag-options a').click(function(e) {
     var nowItem = e.target.closest('.item');
     JDA.setTagsType($(nowItem).data('type'));
     $(nowItem).addClass('selected');
     $(nowItem).siblings('.item').removeClass('selected');
     JDA.setTags()
   });
-  $('.f-btnbox .item[data-type=' + JDA.getTagsType() + ']').addClass('selected');
+  $('.jda-tag-options .item[data-type=' + JDA.getTagsType() + ']').addClass('selected');
   // tags count
   var node = '<div style="float: left; padding-top:2px;" ><select class="jda-auto-tags-count">';
   for (var i = 0; i < 5; i++) {
     node += '<option value="' + (i+1) + '">' + (i + 1) + '条</option>';
   }
   node += '</select></div>';
-  $('.f-btnbox .jda-tag-options').append(node);
+  $('.jda-tag-options').append(node);
   $('.jda-auto-tags-count option:eq(' + (JDA.getTagsCount()-1) + ')').attr('selected', 'selected');
   $('.jda-auto-tags-count').change(function(e) {
     JDA.setTagsCount(parseInt($(e.target).find(':selected').attr('value')));
     JDA.setTags()
+  });
+  // cmts
+  $(appendNode).append('<div class="dd jda-cmts-options jda-options" style="float:right; margin-top: 9px; margin-right: 9px; "><span style="float:left; padding-right:10px; font-size:15px; padding-top:2px;">评论选项:</span><div class="item" data-type="random"><a href="#none">随机评论</a></div> <div class="item" data-type="ordered"><a href="#none">选定评论</a></div></div>');
+  $(appendNode).append('<style type="text/css">' +
+    '.jda-options .item {margin: 2px 8px 2px 0; float: left;}' +
+    '.jda-options .item a {padding:6px 8px; border:1px solid #ccc;  background: #fff; font-size: 13px;}' +
+    '.jda-options .item.selected a {border: 2px solid #e4393c; padding: 4px 6px;}' +
+    '</style>');
+  $('.jda-cmts-options a').click(function(e) {
+    var nowItem = e.target.closest('.item');
+    JDA.setCmtsType($(nowItem).data('type'));
+    $(nowItem).addClass('selected');
+    $(nowItem).siblings('.item').removeClass('selected');
+    JDA.setAllCmtTextarea()
+  });
+  $('.jda-cmts-options .item[data-type=' + JDA.getCmtsType() + ']').addClass('selected');
+  $('.jda-cmts-options').append(JDA.cmtsSelectNodeString(false));
+  $('.jda-cmts-options select').change(function(e) {
+    JDA.setOrderCmt(parseInt($(e.target).find(':selected').data('order')));
+    if (JDA.getCmtsType == 'ordered') {}
+    JDA.setAllCmtTextarea()
   });
 }
 
@@ -241,7 +298,7 @@ JDA.addChangeCmtBar = function() {
     '</div>');
   nodeString += '</div>'
   $('.mycomment-form').append(nodeString);
-  $('.mycomment-form').append('<style type="text/css">' +
+  $('.mycomment-form').append('<style id="jda-change-cmt-bar-css" type="text/css">' +
     '.cmt-item {width: 100%; text-align: center; padding-top: 20px;}' +
     '.jda-change-cmt-bar input {width:500px; height: 20px; padding: 3px;}' +
     '.jda-change-cmt-bar a {width: 100px; height: 25px; line-height:25px; margin-top:3px; margin-left:10px; background-color: #696969; border-radius: 3px; display: inline-block; font-size: 15px; color: #fff;}' +
@@ -260,6 +317,7 @@ JDA.addChangeCmtBar = function() {
       cmts[index] = $(el).val();
     });
     JDA.setCmts(cmts);
+    JDA.setCmtsSelect();
   });
   $('.jda-change-cmt-bar .cmt-add').live('click', function(e) {
     e.preventDefault();
@@ -284,6 +342,7 @@ JDA.addChangeCmtBar = function() {
     e.preventDefault();
     JDA.setCmts(JDA.default_cmts);
     $('.jda-change-cmt-bar').remove();
+    $('#jda-change-cmt-bar-css').remove();
     JDA.addChangeCmtBar();
     JDA.setCmtsSelect();
   });
